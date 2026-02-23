6ace86ec3e396f58397bf3c112f0c94bf7b93dc8
yogireddy <yogireddy8010@gmail.com>
2026-02-23 12:49:49 +0530

    Initial commit

--- src/test/java/practice/AutomateSpider.java
+++ src/test/java/practice/AutomateSpider.java
@@ -3,9 +3,12 @@ package practice;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.MouseButton;
+import com.microsoft.playwright.options.SelectOption;
 import org.testng.annotations.Test;
 
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -145,7 +148,7 @@ public class AutomateSpider extends BaseOne {
         }
     }
 
-    @Test(priority = 5,enabled = false)
+    @Test(priority = 5, enabled = false)
     public void handleRadioButton() throws InterruptedException {
         page.locator("//section[text()='Radio Button']").click();
         page.locator("//h1[text()='Checkout Page']").waitFor();
@@ -161,9 +164,174 @@ public class AutomateSpider extends BaseOne {
         }
     }
 
-    @Test(priority = 6)
-    public void handleImages(){
+    @Test(priority = 6, enabled = false)
+    public void handleImages() {
+        page.locator("//section[text()='Image']").click();
+        Locator img = page.locator("//img[@title=\"Image tooltip\"]");
+        img.waitFor();
+        int naturalWidth = (int) img.evaluate("img => img.naturalWidth");
+        int naturalHeight = (int) img.evaluate("img => img.naturalHeight");
+        System.out.println("Natural Width: " + naturalWidth);
+        System.out.println("Natural Height: " + naturalHeight);
 
+        page.locator("//a[text()='Clickable Image']").click();
+        Locator clickableImg = page.locator("//a[@title=\"Mens Clothing\"]");
+//        int naturalWidth1 = (int) clickableImg.evaluate("img => img.naturalWidth");
+//        int naturalHeight1 = (int) clickableImg.evaluate("img => img.naturalHeight");
+//        System.out.println("Natural Width: " + naturalWidth1);
+//        System.out.println("Natural Height: " + naturalHeight1);
+        clickableImg.click();
+        Locator getItemName = page.locator("//h2[@class=\"font-bold text-xl pb-3\"]");
+        System.out.println(getItemName.textContent());
+
+        page.locator("//a[text()='Broken Image']").click();
+        Locator brokenImg = page.locator("//section[@class=\"flex gap-6\"]/figure/img");
+        page.locator("(//figure[@class=\"border-[1px] basis-[22%] shadow\"])[4]").waitFor();
+
+        for (int i = 0; i < brokenImg.count(); i++) {
+            Locator getImg = brokenImg.nth(i);
+
+            int naturalWidth2 = (int) getImg.evaluate("img => img.naturalWidth");
+
+            if (naturalWidth2 == 0) {
+                System.out.println("Broken Image Found: " + getImg.getAttribute("src"));
+            } else {
+                System.out.println("Image loaded: " + getImg.getAttribute("src"));
+
+            }
+
+        }
+
+    }
+
+    @Test(priority = 7, enabled = false)
+    public void handleDropdown() throws InterruptedException {
+        page.locator("//section[text()='Dropdown']").click();
+
+        Locator selCountry = page.locator("#select3");
+        for (int i = 0; i < selCountry.count(); i++) {
+            Locator getItems = selCountry.nth(i);
+            System.out.print(getItems.textContent());
+        }
+        System.out.println();
+        selCountry.selectOption(new SelectOption().setLabel("India"));
+        sleep(500);
+
+        Locator selState = page.locator("#select5");
+        for (int i = 0; i < selState.count(); i++) {
+            Locator getItems = selState.nth(i);
+            System.out.print(" | " + getItems.textContent());
+        }
+        System.out.println();
+        selState.selectOption(new SelectOption().setValue("Andhra Pradesh"));
+        sleep(500);
+
+        Locator selCity = page.locator("(//select[contains(@class, 'transition-all duration-150')])[4]");
+        for (int i = 0; i < selCity.count(); i++) {
+            Locator getItems = selCity.nth(i);
+            System.out.println(getItems.textContent());
+        }
+        System.out.println();
+        selCity.selectOption(new SelectOption().setIndex(14));
+        sleep(500);
+        page.locator("#continuebtn").click();
+
+        //Multi Select Dropdown
+        page.locator("//a[text()='Multi Select']").click();
+        Locator multiSelect = page.locator("#select-multiple-native");
+        multiSelect.selectOption(new SelectOption().setLabel("Solid Gold Petite Mi..."));
+        sleep(500);
+        multiSelect.selectOption(new SelectOption().setLabel("Mens Casual Slim Fit..."));
+        sleep(500);
+        page.locator("//button[@class=\"bg-orange-500 p-2 text-white rounded w-[150px]\"]").click();
+
+    }
+
+    @Test(priority = 8, enabled = false)
+    public void handleWebTables() throws InterruptedException {
+
+        page.locator("text=Web Table").click();
+        sleep(500);
+        Locator table = page.locator("table.w-full");
+        // Headers
+        Locator headers = table.locator("thead th");
+        for (int i = 0; i < headers.count(); i++) {
+            System.out.print(headers.nth(i).innerText() + " | ");
+        }
+        System.out.println();
+        // Rows
+        Locator rows = table.locator("tbody tr");
+        for (int i = 0; i < rows.count(); i++) {
+            Locator cells = rows.nth(i).locator("td");
+            for (int j = 0; j < cells.count(); j++) {
+                System.out.print(cells.nth(j).innerText() + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    @Test(priority = 9, enabled = false)
+    public void handleFormValidation() throws InterruptedException {
+
+        page.locator("text=FormValidation").click();
+        Locator subBtn = page.locator("//button[@class=\"bg-orange-600 rounded-md text-white py-2 px-4 mx-2 ms-0\"]");
+        subBtn.click();
+        Locator error = page.locator("//p[@class=\"text-red-500 text-sm\"]");
+        int errorCount = error.count();
+        for (int i = 0; i < errorCount; i++) {
+            Locator getTxt = error.nth(i);
+            System.out.println((i + 1) + " | " + getTxt.textContent());
+        }
+        page.locator("#fullName").fill("Peddi");
+        final String EMAIL_REGEX =
+                "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        final String PHONE_REGEX = "^[6-9]\\d{9}$";
+
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher("peddi123@gmail.com");
+        page.locator("#emailId").fill("peddi123@gmail.com");
+        matcher.matches();
+        page.locator("#password").fill("Peddi@123");
+        Pattern phnPattern = Pattern.compile(PHONE_REGEX);
+        Matcher phnMatcher = phnPattern.matcher("9875463210");
+        phnMatcher.matches();
+        page.locator("#mobile").fill("9875463210");
+        Locator dropdown = page.locator("#city");
+        dropdown.selectOption(new SelectOption().setValue("hyd"));
+        page.setInputFiles("#resume", Paths.get("C:\\Users\\yogireddy\\Downloads\\XPATH.pdf"));
+
+        Locator multiSel = page.locator("//select[contains(@class, 'MuiNativeSelect-select MuiNativeSelect-outlined')]");
+        multiSel.selectOption(new String[]{"CSS", "HTML", "Javascript"});
+        subBtn.click();
+        System.out.println("Error count | " + errorCount);
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Form Registration!!.png")));
+
+
+    }
+
+    @Test(priority = 9, enabled = true)
+    public void scrollThePage() throws InterruptedException {
+        page.locator("(//section[text()=\"Scroll\"])[1]").click();
+        page.locator("(//section[text()=\"Scroll\"])[2]").click();
+        Page newTab = page.waitForPopup(() ->
+                page.locator("//a[@href=\"/ui/scroll/newTabVertical\"]").click()
+        );
+       // newTab.evaluate("window.scrollTo(0, document.body.scrollHeight)");
+        newTab.keyboard().press("End");
+        Locator checkbox =   newTab.locator("//input[@type=\"checkbox\"]");
+        checkbox.scrollIntoViewIfNeeded();
+        checkbox.check();
+        newTab.locator("//button[text()='Accept Our Policy']").click();
+        newTab.close();
+
+        page.locator("//a[@href=\"/ui/scroll/newHorizontal\"]").click();
+
+        Page horTab = page.waitForPopup(() ->
+                page.locator("//a[@href=\"/ui/scroll/newTabHorizontal\"]").click()
+                );
+        horTab.evaluate("window.scrollBy(1000,0)");
+        sleep(2000);
+        horTab.close();
     }
 
     private void sleep(int time) throws InterruptedException {
